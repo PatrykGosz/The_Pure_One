b173b17c76861fcaf61cf9f5a1ac9215c13b70db
MateuszKarolChrusciel <mateuszkarolchrusciel@gmail.com>
2019-08-10 15:37:54 +0200

    Launcher updated

--- file4testing.py
+++ file4testing.py
@@ -1,5 +1,4 @@
 from saved_games import *
 
 if __name__ == '__main__':
-    a = saved_games.saves.save_02.NAME
-
+    try
--- launcher.py
+++ launcher.py
@@ -10,11 +10,10 @@ class main_menu:
 
     def menu(self):
         print("\nThe Pure One \n")
-        print("Please select from the below options:")
         print("1. Continue")
         print("2. New game")
         print("3. Saved games")
-        users_selections = input("\nYour selections: ")
+        users_selections = input("\nYour selection: ")
 
         if users_selections == "1":
             self._continue_game()
@@ -33,9 +32,18 @@ class main_menu:
         pass  # TODO
 
     def _choose_save_file(self):
-        pass  # TODO
+        all_saves_names = self._print_saved_files()
+        counter = 0
+        print("\nSaves:\n")
+        for name in all_saves_names:
+            counter += 1
+            print(f"{counter}. {name}")
+        print(f"\n{counter + 1}. Return")
+        print("\nYour selection: ")
+        # TODO
 
     def _print_saved_files(self):
+        save_name_all = []
         list_of_saved_files = self._saved_files()
         for file in list_of_saved_files:
             if file.split("_")[0] != "save":
@@ -45,9 +53,10 @@ class main_menu:
                     with open(f"saved_games/{file}") as temp_file:
                         save_name_line = temp_file.readline()
                         save_name = save_name_line.split(" ")[2]
-                        print(save_name)
+                        save_name_all.append(save_name)
                 finally:
                     pass  # TODO
+        return save_name_all
 
     def _saved_files(self):
         list_of_saved_files = os.listdir(self.SAVED_FILES_PATH)
